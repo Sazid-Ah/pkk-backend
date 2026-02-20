9331f4e1641a2075b9518c50375d10bfcdec9748
Manish Kumar <manishsinghjnv11@gmail.com>
2026-02-20 21:06:07 +0530

    20-02-2026 21:06

--- models/Pandit.js
+++ models/Pandit.js
@@ -29,63 +29,9 @@ const panditSchema = new mongoose.Schema({
     occasions: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Occasion'
-    }],
-    // Geolocation fields
-    location: {
-        address: {
-            type: String,
-            trim: true,
-        },
-        street: String,
-        city: String,
-        state: String,
-        zip: String,
-        landmark: String,
-        country: {
-            type: String,
-            default: 'India'
-        },
-        coordinates: {
-            type: {
-                type: String,
-                enum: ['Point'],
-                default: 'Point'
-            },
-            coordinates: {
-                type: [Number], // [longitude, latitude]
-                default: [0, 0]
-            }
-        }
-    },
-    // Service area
-    availableRadius: {
-        type: Number, // in kilometers
-        default: 15,
-        min: 1,
-        max: 100
-    },
-    // Additional details
-    experience: {
-        type: Number, // years of experience
-        default: 1
-    },
-    bio: String,
-    languages: [String],
-    specializations: [String],
-    phone: String,
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    totalBookings: {
-        type: Number,
-        default: 0
-    }
+    }]
 }, {
     timestamps: true,
 });
-
-// Create geospatial index for location-based queries
-panditSchema.index({ 'location.coordinates': '2dsphere' });
 
 module.exports = mongoose.model('Pandit', panditSchema);
